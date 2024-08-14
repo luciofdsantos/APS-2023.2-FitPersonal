20542952edcbf9034b2d4ba52ba07ddc48e1407b
Lorena Avelino <lorenaavelino28@gmail.com>
2024-08-13 23:35:39 -0300

    feat(front-end): adicionando rota para arquivo Planos Alimentares

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from '../screens/Dashboard';
 import Treinos from '../screens/Treinos';
+import PlanosAlimentares from '../screens/PlanosAlimentares';
 
 const AppRoutes: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const AppRoutes: React.FC = () => {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/treinos" element={<Treinos />} />
+        <Route path="/planos-alimentares" element={<PlanosAlimentares />} />
       </Routes>
     </Router>
   );
