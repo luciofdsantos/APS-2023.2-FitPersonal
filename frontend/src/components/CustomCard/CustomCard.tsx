5ff7a728e4fe5dcb899bc1af519f32f9fd7b9a6c
Deise Santana <dsds3@aluno.ifnmg.edu.br>
2024-08-15 22:05:40 -0300

    feat(frontend): adicionado query

--- frontend/src/components/CustomCard/CustomCard.tsx
+++ frontend/src/components/CustomCard/CustomCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, Typography, CardActions } from '@mui/material';
+import { Card, CardContent, Typography, Box } from '@mui/material';
 import GroupButtons from '../GroupButtons';
 import { TypeCard } from 'src/types';
 
@@ -10,31 +10,41 @@ export default function CustomCard({
 }: TypeCard.CustomCardProps) {
   return (
     <Card sx={style}>
-      <CardContent>
-        <Typography
-          gutterBottom
-          variant="h5"
-          component="div"
-          sx={{ color: '#6842FF', fontSize: '20px' }}
+      <CardContent sx={{ marginBottom: '-10px' }}>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'flex-start'
+          }}
         >
-          {title}
-        </Typography>
-        {items.map((item, index) => (
-          <Typography
-            key={index}
-            variant="body2"
-            color="text.secondary"
-            sx={{ color: '#FFFFFF' }}
-          >
-            {item.label}: {item.value}
-          </Typography>
-        ))}
+          <Box sx={{ flex: 1 }}>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              sx={{ color: '#6842FF', fontSize: '20px' }}
+            >
+              {title}
+            </Typography>
+            {items.map((item, index) => (
+              <Typography
+                key={index}
+                variant="body2"
+                color="text.secondary"
+                sx={{ color: '#FFFFFF' }}
+              >
+                {item.label}: {item.value}
+              </Typography>
+            ))}
+          </Box>
+          {buttons && (
+            <Box sx={{ ml: 2, mt: 2 }}>
+              <GroupButtons buttons={buttons} />
+            </Box>
+          )}
+        </Box>
       </CardContent>
-      {buttons && (
-        <CardActions sx={{ justifyContent: 'flex-end' }}>
-          <GroupButtons buttons={buttons} />
-        </CardActions>
-      )}
     </Card>
   );
 }
--- frontend/src/screens/Treinos/Treinos.tsx
+++ frontend/src/screens/Treinos/Treinos.tsx
@@ -1,34 +1,97 @@
 import { GroupButtons, CustomCard, CustomLayout } from '../../components';
-// import { useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import Dashboard from '@mui/icons-material/Dashboard';
 import Grid from '@mui/material/Grid';
 import EditIcon from '@mui/icons-material/Edit';
 
 const items = [{ text: 'Dashboard', Icon: Dashboard, path: '/' }];
 
-// const endpoint = 'http://localhost:8080/api/treinos';
+const endpoint = 'http://149.100.154.48:3000/treinos';
 
 export default function Novo() {
-  // const { data, isFetching } = useQuery({
-  //   queryKey: ['treinos'],
-  //   queryFn: async () => {
-  //     const response = await fetch(endpoint);
-  //     if (!response.ok) {
-  //       const errorMessage = await response.text();
-  //       throw new Error(`Erro ao buscar treinos: ${errorMessage}`);
-  //     }
-  //     return response.json();
-  //   }
-  // });
+  const { data, isFetching } = useQuery({
+    queryKey: ['treinos'],
+    queryFn: async () => {
+      const response = await fetch(endpoint);
+      if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(`Erro ao buscar treinos: ${errorMessage}`);
+      }
+      return response.json();
+    }
+  });
 
-  // console.log('data -> ', isFetching);
-  // console.log('data -> ', data);
+  console.log('data -> ', isFetching);
+  console.log('data -> ', data);
 
   return (
     <CustomLayout appBarText="Treinos" items={items}>
       <GroupButtons buttons={[{ text: 'Novo Treino', href: '/novo-treino' }]} />
 
       <Grid container spacing={3}>
+        <Grid item xs={12} md={8} lg={4}>
+          <CustomCard
+            title="Treino Teste"
+            items={[{ label: 'Descrição', value: 'Descrição para teste' }]}
+            style={{
+              backgroundColor: '#1F2229',
+              borderRadius: '16px',
+              boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)'
+            }}
+            buttons={[
+              {
+                startIcon: <EditIcon />,
+                href: '/editar-treino',
+                backgroundColor: 'transparent',
+                iconColor: '#6842FF',
+                border: 'none'
+              }
+            ]}
+          />
+        </Grid>
+
+        <Grid item xs={12} md={8} lg={4}>
+          <CustomCard
+            title="Treino Teste"
+            items={[{ label: 'Descrição', value: 'Descrição para teste' }]}
+            style={{
+              backgroundColor: '#1F2229',
+              borderRadius: '16px',
+              boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)'
+            }}
+            buttons={[
+              {
+                startIcon: <EditIcon />,
+                href: '/editar-treino',
+                backgroundColor: 'transparent',
+                iconColor: '#6842FF',
+                border: 'none'
+              }
+            ]}
+          />
+        </Grid>
+
+        <Grid item xs={12} md={8} lg={4}>
+          <CustomCard
+            title="Treino Teste"
+            items={[{ label: 'Descrição', value: 'Descrição para teste' }]}
+            style={{
+              backgroundColor: '#1F2229',
+              borderRadius: '16px',
+              boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)'
+            }}
+            buttons={[
+              {
+                startIcon: <EditIcon />,
+                href: '/editar-treino',
+                backgroundColor: 'transparent',
+                iconColor: '#6842FF',
+                border: 'none'
+              }
+            ]}
+          />
+        </Grid>
+
         <Grid item xs={12} md={8} lg={4}>
           <CustomCard
             title="Treino Teste"
