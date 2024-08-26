ba0218b278d8fc1577b08506a86ade5196662fda
Warley Ramires <warleyramiresgoncalves@gmail.com>
2024-08-26 20:42:42 -0300

    Correção de inputs nulos

--- backend/src/main/java/com/fitpersonal/fitpersonal/entities/dtos/UsuarioRequestDTO.java
+++ backend/src/main/java/com/fitpersonal/fitpersonal/entities/dtos/UsuarioRequestDTO.java
@@ -1,5 +1,6 @@
 package com.fitpersonal.fitpersonal.entities.dtos;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -8,7 +9,9 @@ public class UsuarioRequestDTO {
     private String sobrenome;
     private String email;
     private String senha;
+    @NotNull(message = "O tipo de usuário é obrigatório.")
     private String tipoUsuario; // "ALUNO", "PERSONAL" ou "NUTRICIONISTA"
+    @NotNull(message = "O sexo é obrigatório.")
     private String sexo; // "MASCULINO, "FEMININO"
     private String registroProfissional;
 }
